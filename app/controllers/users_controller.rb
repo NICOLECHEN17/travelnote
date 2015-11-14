178959a5f5b799c71e843518ee572132984b76e2
Jialu Chen <657140380@qq.com>
2015-11-14 23:57:57 +0000

    finish signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@
 
       def new
       end
-    end
+
+      def new
+        @user = User.new
+      end
+      def create
+        secure_params = params.require(:user).permit(:name, :email, 
+                                  :password, :password_confirmation)
+        @user = User.new(secure_params)
+        if @user.save
+        	flash[:success] = "Welcome to Travelnote!" 
+        	redirect_to @user
+           # Handle a successful save.
+        else
+        render 'new'     # NEW LINE      
+        end
+      end
+    
+   end
