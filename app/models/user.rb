9d01a3ef756ec4834e31134b4df883ec0f4041fb
Jialu Chen <657140380@qq.com>
2015-11-15 01:27:30 +0000

    Finish log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
 	before_save { self.email = email.downcase }
       validates :name, presence: true, length: { in: 5..30 }
       VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
 
-          
-          resources :users
+          resources :users  
           root 'static_pages#home'
 
           
           get 'about'   => 'static_pages#about'
           get 'contact' => 'static_pages#contact'
-          get 'signup'  => 'users#new'
-          get    'login'   => 'sessions#new'
-          post   'login'   => 'sessions#create'
-          delete 'logout'  => 'sessions#destroy'
+           get 'signup'  => 'users#new'
+           get    'login'   => 'sessions#new'
+      post   'login'   => 'sessions#create'
+      delete 'logout'  => 'sessions#destroy'
          
 
       
